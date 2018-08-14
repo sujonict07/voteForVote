bb6eeb1866851619e28016bb65b76a63c5c73781
sujon <sujonict07@gmail.com>
2018-08-14 19:06:17 +0600

    Added templetes and others modules

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
+import datetime
 # Create your models here.
 
 
@@ -12,6 +14,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 
 @python_2_unicode_compatible
 class Choice(models.Model):
@@ -21,4 +27,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-    
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,38 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.template import loader
+from .models import Question
 
 
 def index(requset):
-    return HttpResponse("Hello world. U are the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # output = ', '.join(q.question_text for q in latest_question_list)
+    # return HttpResponse(template.render(context, requset))
+    return render(requset, 'polls/index.html',context)
+
+
+def details(request, question_id):
+    # return HttpResponse("You are looking at Question no {}".format(question_id))
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.doesNotExist:
+    #     raise Http404("Question does not exists.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question':question})
+
+
+
+def results(request, question_id):
+    return HttpResponse("You are looking results at Question no {}".format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting at Question no {}".format(question_id))
+
+
+
